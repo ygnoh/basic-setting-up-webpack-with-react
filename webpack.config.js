246ef3c2ef6a2d4c6f35084ad027845b24014889
yonggoonoh <nohyg90@gmail.com>
2018-07-24 03:15:29 +0900

    set up babel-loader to convert jsx to js

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
     }
 };
