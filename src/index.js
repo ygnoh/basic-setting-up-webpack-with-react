8de5ef6f24bf26a301d3db2d041a5dbd8644a7b1
yonggoonoh <nohyg90@gmail.com>
2018-07-24 03:15:56 +0900

    create simple component and render it to root

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return <h1>Hello world</h1>;
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
